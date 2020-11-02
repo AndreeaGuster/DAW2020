ed2e94ffe8a2e075495b468ca145b89934fa4c2b
Guster Andreea Daniela <guster_andreea@yahoo.com>
2020-11-02 23:12:27 +0200

    added lab 5 solution

---
+++ lab5/lab3_miercuri/lab3_miercuri/Models/BookType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab3_miercuri.Models
+{
+    public class BookType
+    {
+        public int BookTypeId { get; set; }
+
+        [MinLength(2, ErrorMessage = "Book type name cannot be less than 2!"),
+        MaxLength(20, ErrorMessage = "Book type name cannot be more than 20!")]
+        public string Name { get; set; }
+
+        // many to one
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}

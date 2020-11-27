fce15829aa541fb887a3877207faa3cf93821055
Guster Andreea Daniela <guster_andreea@yahoo.com>
2020-11-27 08:04:49 +0200

    added lab 8

--- lab6/lab6/Models/IdentityModels.cs
+++ lab6/lab6/Models/IdentityModels.cs
@@ -24,7 +24,7 @@ namespace lab6.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-
+		
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
